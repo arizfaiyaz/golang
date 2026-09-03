f9fbd07d9e01c66f6f970fa23b56b2fd20c436ad
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-09-04 00:55:16 +0530

    constant, values, variables

--- function.go
+++ function.go
@@ -13,9 +13,24 @@ func plus(a,b,c int) int {
 	return a + b + c
 }
 
+func vals() (int, int){
+	return 3, 7
+}
+
 func main() {
 	res := sum(1, 2)
 	fmt.Println(res)
 	res2 := plus(1, 2, 3)
 	fmt.Println(res2)
+
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c, _ := vals()
+	fmt.Println(c)
 }
+
+// Function with multiple return values
+// func vals() (int, int){}
+// 
